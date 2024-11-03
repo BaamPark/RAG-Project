5fe93411294d844f266948ed04ec1788d791a35c
BaamPark <baam.park.95@gmail.com>
2024-11-02 21:21:34 -0400

    rename bash file

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 # Function to stop all background processes on Ctrl+C
 cleanup() {
   echo "Stopping servers..."
-  pkill -f "python backend/main.py"  # Stop backend
+  pkill -f "python /app/backend/main.py"  # Stop backend
   pkill -f "npm run dev"  # Stop frontend
   exit 0
 }
@@ -10,12 +10,12 @@ cleanup() {
 trap cleanup SIGINT
 
 # Start the backend (FastAPI)
-echo "Starting FastAPI backend..."
-python backend/main.py &  # Run backend in the background
+echo "Starting FastAPI backend on port 8000..."
+python /app/backend/main.py &  # Run backend in the background
 
 # Start the frontend (Next.js)
-echo "Starting Next.js frontend..."
-npm --prefix frontend run dev &  # Run frontend from the root
+echo "Starting Next.js frontend on port 3000..."
+npm --prefix /app/frontend run dev &  # Run frontend from the root
 
 # Wait for both processes to finish
 wait
